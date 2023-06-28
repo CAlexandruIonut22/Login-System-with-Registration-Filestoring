17e30e8bdfc2c25e6a8a2c3e766a38bdbaf3a768
costa <costa@DESKTOP-BKU93S9>
2023-06-28 20:31:21 +0300

    General purpose code

--- main.cpp
+++ main.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+
 using namespace std;
 
 
-bool isLoggedin(){
-    string username,password, un, pw;
-    cout<<"Enter username: ";cin>>username;
-    cout<<"Enter password: ";cin>>password;
+bool isLoggedin() {
+    string foldername, username, password, un, pw;
+    cout << "Enter username: ";
+    cin >> username;
+    cout << "Enter password: ";
+    cin >> password;
 
-    ifstream read("C:\\Users\\costa\\CLionProjects\\LoginSystem\\"+ username + ".txt");
+    ifstream read(foldername + username + ".txt");
     getline(read, un);
     getline(read, pw);
 
-    if (un == username && pw == password){
+    if (un == username && pw == password) {
         return true;
-    }
-    else {
+    } else {
         return false;
     }
 }
@@ -24,26 +26,28 @@ bool isLoggedin(){
 
 int main() {
     int choice;
-    cout<<"1: Register\n2: Login\nYour choice: ";cin>>choice;
-    if (choice == 1){
+    string foldername;
+    cout << "1: Register\n2: Login\nYour choice: ";
+    cin >> choice;
+    if (choice == 1) {
         string username, password;
-        cout<<"Select a username: ";cin>>username;
-        cout<<"Select a password: ";cin>>password;
+        cout << "Select a username: ";
+        cin >> username;
+        cout << "Select a password: ";
+        cin >> password;
         ofstream file;
-        file.open("C:\\Users\\costa\\CLionProjects\\LoginSystem\\"+ username + ".txt");
+        file.open(foldername + username + ".txt" + username + ".txt");
         file << username << endl << password;
         file.close();
         main();
-    }
-    else if (choice == 2) {
+    } else if (choice == 2) {
         bool status = isLoggedin();
-        if (!status){
-            cout << "Login failed!"<<endl;
+        if (!status) {
+            cout << "Login failed!" << endl;
             system("PAUSE");
             return 0;
-        }
-        else{
-            cout << "Login succesful!"<<endl;
+        } else {
+            cout << "Login succesful!" << endl;
             system("PAUSE");
             return 1;
         }
